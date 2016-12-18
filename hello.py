e369eac46d39800a1465062055fa82782e5ed17e
Zonov Sergey <st.apostol@mail.ru>
2016-12-18 18:51:05 +0300

    End of fourth chapter

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -21,6 +21,9 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
