02147da2c4c3f796cbc685391abb1dc9a00d2676
Zonov Sergey <st.apostol@mail.ru>
2016-12-18 18:18:28 +0300

    4b

--- hello.py
+++ hello.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template
-# from flask.ext.bootstrap import Bootstrap
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
-# from flask.ext.moment import Moment
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.errorhandler(404)
